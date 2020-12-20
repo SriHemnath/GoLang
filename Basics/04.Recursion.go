020586d9b1bd99a775aa00e06b8b225b0d35ea68
HEMNATH-PC\Hemnath <hemnath.j1995@gmail.com>
2020-12-20 15:55:53 +0530

    recurison using channels

---
+++ Basics/04.Recursion.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println(fib(0,1,10))
+}
+
+func fib(a,b,upTo int) int  {
+	out:=make(chan int)
+	go fibR(a,b,upTo,out)
+	fmt.Println("Resuming main function")
+	return <-out
+}
+
+func fibR(a,b,upTo int,ch chan<-int){
+	fmt.Println("goroutine fibR running")
+	c:=a+b
+	if c>=upTo{
+		ch <-c
+		fmt.Println("Returning conrol to main")
+		return
+	}
+	go fibR(b,c,upTo,ch)
+}
