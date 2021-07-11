3dfe477308e79c8af980eb43d0e0e66b33886685
HEMNATH-PC\Hemnath <hemnath.j1995@gmail.com>
2021-07-11 13:29:11 +0530

    test

--- Basics/02_IOTA.go
+++ Basics/02_IOTA.go
@@ -2,32 +2,24 @@ package main
 
 import "fmt"
 
-const(
-	a = iota +1
+const (
+	a = iota + 1
 	b
 	c
 )
 
-const(
-	d,e,f=iota,iota,iota
+const (
+	d, e, f = iota, iota, iota
 )
 
-func main(){
-	fmt.Println(a,b,c,d,e,f)
+func main() {
+	fmt.Println(a, b, c, d, e, f) // 1 2 3 0 0 0
 	fmt.Println("IOTA starts from 0 and increment value after each specification. Values are reset in new declaration")
 
-	//only IF & FOR loop available in GO.
-	//using for as while
-	v:=1
-	for  v < 10  {
-		fmt.Println("something")
-		v++
-	}
-
 	//we range over a collection of values using for range
-	s:=[]int{1,2,3,4,5,6}
-	for _, val := range s {
-		fmt.Println(val)
+	s := []int{1, 2, 3, 4, 5, 6}
+	for index, val := range s {
+		fmt.Println("Value at index ", index, "is ", val)
 	}
 
 	fmt.Println("Printing prime numbers")
@@ -35,7 +27,7 @@ func main(){
 }
 
 //PRINT PRIME
-func printPrime(){
+func printPrime() {
 	for i := 2; i <= 100; i++ {
 		if isPrime(i) {
 			fmt.Println(i)
@@ -43,14 +35,11 @@ func printPrime(){
 	}
 }
 
-func isPrime(n int) bool{
-	if n<=1{
-		return false
-	}
+func isPrime(n int) bool {
 	for i := 2; i < n; i++ {
-		if n % i == 0 {
+		if n%i == 0 {
 			return false
-		}			
+		}
 	}
 	return true
 }
