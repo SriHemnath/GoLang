e42031428e6caf5b66177442852caf5c4d56fd83
HEMNATH-PC\Hemnath <hemnath.j1995@gmail.com>
2021-12-27 15:03:50 +0530

    hackerrank solution for amazon

---
+++ Basics/04.Problems/05.AmazonValidCoupon/README.md
@@ -0,0 +1,9 @@
+## Hackerearth Amazon question
+
+Employees are tasked with generating valid discount coupons for customer.
+
+The validity of a discount coupon is determined as follows:
+1. An empty discount coupon is valid.
+2. If a discount coupon A is valid, then a discount coupon C made by adding one character x
+to both the beginning of A and the end of A is also valid (C=xAx is valid).
+3. If two discount coupons A and B are valid, then the concatenation of B and A is also valid (AB and BA are both valid)
---
+++ Basics/04.Problems/05.AmazonValidCoupon/main.go
@@ -0,0 +1,60 @@
+package main
+
+import "fmt"
+
+type Stack []string
+
+func (s *Stack) Push(str string) {
+	*s = append(*s, str)
+}
+
+func (s *Stack) isEmpty() bool {
+	return len(*s) == 0
+}
+
+func (s *Stack) Pop() (string, bool) {
+	if s.isEmpty() {
+		return "", false
+	}
+	index := len(*s) - 1
+	element := (*s)[index]
+	*s = (*s)[:index]
+	return element, true
+}
+
+func (s *Stack) Peek() string {
+	if s.isEmpty() {
+		return ""
+	}
+	index := len(*s) - 1
+	element := (*s)[index]
+	return element
+}
+
+func main() {
+	// var first string
+	// fmt.Scanln(&first)
+	fmt.Println(isValid(""))
+	fmt.Println(isValid("vv"))
+	fmt.Println(isValid("xbbx"))
+	fmt.Println(isValid("bbccdd"))
+	fmt.Println(isValid("xyffyxdd"))
+	fmt.Println(isValid("xyx"))
+	fmt.Println(isValid("bbb"))
+	fmt.Println(isValid("abc"))
+}
+
+func isValid(s string) bool {
+	var stack Stack
+	for i := 0; i < len(s); i++ {
+		c := fmt.Sprintf("%c\n", s[i])
+		if stack.isEmpty() {
+			stack.Push(c)
+		} else if stack.Peek() == c {
+			stack.Pop()
+		} else {
+			stack.Push(c)
+		}
+	}
+	return stack.isEmpty()
+}
