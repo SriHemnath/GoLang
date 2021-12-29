23c3e45b973a63f358b12cc11b08c49c75239e8d
HEMNATH-PC\Hemnath <hemnath.j1995@gmail.com>
2021-12-29 14:06:29 +0530

    solved word suggestion

--- Basics/04.Problems/01.WordSuggetion/WordSuggestion.go
+++ Basics/04.Problems/01.WordSuggetion/WordSuggestion.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 	"sort"
+	"strings"
 )
 
 func suggestedProducts(products []string, searchWord string) [][]string {
@@ -68,7 +70,35 @@ func main() {
 	sort.Slice(products, func(i, j int) bool {
 		return products[i] < products[j]
 	})
-	output := suggestedProducts(products, "bags")
-	fmt.Println(output)
+	//output := suggestedProducts(products, "bags")
+	//fmt.Println(output)
+	h := "Hemnath"
+	sliceString := h[0:2]
+	fmt.Println(sliceString, reflect.TypeOf(sliceString))
+	fmt.Println(strings.HasPrefix(sliceString, "e"))
+	fmt.Println(strings.HasPrefix(sliceString, "h"))
+	fmt.Println(strings.HasPrefix(sliceString, "H"))
+	fmt.Println(suggestedProducts2(products, "bags"))
+	fmt.Println(suggestedProducts2([]string{"mobile", "mouse", "moneypot", "monitor", "mousepad"}, "mouse"))
+	fmt.Println(suggestedProducts2([]string{"havana"}, "havana"))
+}
 
+//more memory efficient and simple soltion
+func suggestedProducts2(products []string, searchWord string) [][]string {
+	n := len(products)
+	sort.Strings(products)
+	var prefix string
+	var res [][]string
+	for _, ch := range searchWord {
+		prefix += string(ch)
+		j := sort.SearchStrings(products, prefix)
+		var curr []string
+		for i := 0; i < 3 && i+j < n; i++ {
+			if strings.HasPrefix(products[i+j], prefix) {
+				curr = append(curr, products[i+j])
+			}
+		}
+		res = append(res, curr)
+	}
+	return res
 }
