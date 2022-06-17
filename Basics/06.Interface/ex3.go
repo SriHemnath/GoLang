5c761dafb6ed5731f59a1896e64beb43bef5cf27
HEMNATH-PC\Hemnath <hemnath.j1995@gmail.com>
2022-06-17 10:18:26 +0530

    playing with interface

--- Basics/06.Interface/ex3.go
+++ Basics/06.Interface/ex3.go
@@ -7,20 +7,27 @@ type check struct {
 
 func main() {
 	var i *int        //i=nil
-	var k int         //k=0
 	var j interface{} //j=nil
+	var k int         //k=0
 	var l *check      //j=nil
 	p := new(int)     //p is reference and value inside p is nil
 	//*p = 1234
+	println("i:")
 	do(i)
+	println("j:")
 	do(j)
+	println("k:")
 	do(k)
+	println("l:")
 	do(l)
+	println("p:")
 	do(p)
 	println(i, j, k, l, p)
 	fmt.Println(i, j, k, l, p)
 }
 
+//whenever interface receives a value, it will have reference of that value as interface{} is a reference type.
+//if empty interface is passed then it will be nil
 func do(i interface{}) {
 	//var pp *interface{}
 	//println(pp)
